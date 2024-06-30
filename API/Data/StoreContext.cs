971c0817f754a37c783d21de66e39b19dc82f891
Numan Maarij <numanmaarij21@gmail.com>
2024-06-30 16:22:45 +0100

    Added Entity and Data classes + Nuget Packages

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; } // EF will use this name when creating the DB for this entity (Product class)
+    }
+}
---
+++ API/Entities/Product.cs
@@ -0,0 +1,12 @@
+
+namespace API.Entities
+{
+    public class Product
+    {
+        // [Key] another way to tell EF this is the PK 
+        public int Id { get; set; } // EF will make this the PK by default
+
+        public string Name { get; set; }
+
+    }
+}
