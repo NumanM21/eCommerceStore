e1fa3ed45cc43b02aca4b6a0ce58fd5e165e1592
Numan Maarij <numanmaarij21@gmail.com>
2024-07-08 15:39:45 +0100

    Car Seeded JSON migration issue resolved. JSON Typo Error!

--- Infrastructure/Data/Migrations/20240708143909_InitialCreate.Designer.cs
+++ Infrastructure/Data/Migrations/20240708143909_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    [Migration("20240708143611_InitialCreate")]
+    [Migration("20240708143909_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
