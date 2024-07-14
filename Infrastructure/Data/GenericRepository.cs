4b10e33e7efc3137051a7f9b8751f10e5b9b6722
Numan Maarij <numanmaarij21@gmail.com>
2024-07-14 20:28:28 +0100

    Implemented the Generic Repository Methods

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
@@ -17,6 +18,12 @@ namespace Infrastructure.Data
             _context = context;
         }
 
+        public async Task<T> GetEntityWithSpecification(ISpecification<T> specification)
+        { 
+            // Method returns IQueryable -> so can pass this to a method which will execute our Query against the DB
+            return await ApplySpecification(specification).FirstOrDefaultAsync();
+        }
+
         public async Task<T> GetProductByIdAsync(int id)
         {
             return await _context.Set<T>().FindAsync(id); // .Set<> will set the type of whatever we want to get ('T' replaced with type)
@@ -26,5 +33,18 @@ namespace Infrastructure.Data
         {
             return await _context.Set<T>().ToListAsync();
         }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> specification)
+        {
+            return await ApplySpecification(specification).ToListAsync();
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> specification)
+        {
+            // T replaced with our Type (I.e. Product) since we are in our gen repo
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), specification);
+
+        }
+
     }
 }
