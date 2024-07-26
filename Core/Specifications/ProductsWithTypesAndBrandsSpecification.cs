4724b4cdaa57a11d9f7e51158e1fbd04d8018bb8
Numan Maarij <numanmaarij21@gmail.com>
2024-07-26 23:02:26 +0100

    Added Parameter to ProductsWithTypesAndBrandsSpecification -> Error (500) -> need to fix

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
 using Core.Entities;
 
 namespace Core.Specifications
@@ -12,10 +6,11 @@ namespace Core.Specifications
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
         // Can use Ctor without parameters, for getting ALL products (ListAsync)
-        public ProductsWithTypesAndBrandsSpecification()
+        public ProductsWithTypesAndBrandsSpecification(string productsToSort)
         {
             AddToInclude(x => x.ProductType);
             AddToInclude(x => x.ProductBrand);
+            AddOrderBy(x => x.Name); // Default Ordering (so Alphabetical)
         }
 
         // Can use Ctor WITH parameters, to specify the product ID which we want to return (GetProduct) -> Both derive from BaseSpecification class
