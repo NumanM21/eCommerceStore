9eef13632378644b9a3cf5a21c8df651c6b64f57
Numan Maarij <numanmaarij21@gmail.com>
2024-08-07 06:35:46 +0100

    Updated StoreContext to handle Decimal error with SQLite. Current issue is sorting API call NOT actually sorting, just returning default list of products

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -12,9 +12,7 @@ namespace Infrastructure.Data
     {
         
         public StoreContext(DbContextOptions options) : base(options) 
-        {
-
-        }
+        {}
 
         public DbSet<Product> Products { get; set; } // EF will use this name when creating the DB for this entity (Product class)
 
@@ -30,6 +28,29 @@ namespace Infrastructure.Data
             // .GetExecutingAssembley() gets the assembley that contains the code currently executing -> Typically refers to the project where the DbContext is defined
             // -> ApplyConfigurationFromAssembley() will scan this project and classes to find IEntityConfiguration<> and automatically apply these configuration to the model
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); 
+
+            // Change Decimal to DOUBLE (double support by SQLite -> Prevent 500 error)
+            if (Database.ProviderName == "Microsoft.EntityFrameWorkCore.Sqlite")
+            {
+                // This conversion ONLY happens when Sqlite is RETURNING a decimal value BACK (will convert to double at that point)
+
+                // Loop through the entities in our modelbuilder (ALL, including the ones which already are a double/ int)
+                foreach(var entityToReturn in modelBuilder.Model.GetEntityTypes()) // NOTE: modelBuilder is an API in EF Core 
+                {
+                    // Find all properties of the entity in  which they have the type of decimal in our modelBuilder
+                    var prop = entityToReturn.ClrType.GetProperties()
+                    .Where(p => p.PropertyType == typeof(decimal));
+
+                    // Loop through all entities which ARE type of DECIMAL and convert to double
+                    foreach(var property in prop)
+                    {
+                        modelBuilder.Entity(entityToReturn.Name) // Gets the name of the entity type, which is a string representing the CLR class name.
+                        .Property(property.Name) // Specifies the property within the entity type that should be configured. Is the name of the property as a string.
+                        .HasConversion<double>();
+                    }
+                }
+
+            }
         }
 
 
